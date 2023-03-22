5b9715a9e7019d966736eb7c3e3d0fe4c67afa11
julianagonzalezastudillo <68652754+julianagonzalezastudillo@users.noreply.github.com>
2023-03-22 14:44:18 +0100

    add supplementary information to metadata file. Store in an appropiate format network file

---
+++ create_metadata_and_networks.py
@@ -0,0 +1,66 @@
+import os.path
+import pickle
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+
+path = '/Users/juliana.gonzalez/ownCloud/Juli-Javi/'
+fc_path = path + 'fc_matrix/'
+net_path = path + 'net_metric/'
+if not os.path.exists(net_path):
+    os.makedirs(net_path)
+
+# build/complete metadata
+metadata = pd.read_csv(path + 'Ts65Dn_npx_a5IA_metadata.csv')
+genot = metadata["genot"].unique()
+subjects = {genot[0]: list(metadata.loc[metadata['genot'] == genot[0], 'id_mouse']),
+            genot[1]: list(metadata.loc[metadata['genot'] == genot[1], 'id_mouse'])}
+
+G_ctr_min = []
+G_ctr_max = []
+G_a5ia_min = []
+G_a5ia_max = []
+num_nodes = []
+for sub in metadata['id_mouse']:
+    edges_file = fc_path + '{0}_edges.csv'.format(sub)
+    nodes_file = fc_path + '{0}_node_attributes.csv'.format(sub)
+
+    edges = pd.read_csv(edges_file)
+    edges = edges.drop(edges.columns[1], axis = 1)
+    edges = edges.rename(columns = {"pre": "source", "post": "target", "sp_trans_p": "weight"})
+
+    G_ctr = nx.from_pandas_edgelist(edges[edges['treatment'] == 'ctr'], edge_attr = True, create_using = nx.DiGraph())
+    G_a5ia = nx.from_pandas_edgelist(edges[edges['treatment'] == 'a5ia'], edge_attr = True,  create_using = nx.DiGraph())
+
+    # nodes info
+    nodes = pd.read_csv(nodes_file)
+    nodes = nodes.drop(nodes.columns[0], axis=1)
+    # nodes = nodes.set_index('node_id').T.to_dict('tight')
+
+    # add node attributes
+    region = nodes.set_index('node_id')['region'].to_dict()
+    nx.set_node_attributes(G_ctr, region, "region")
+    nx.set_node_attributes(G_a5ia, region, "region")
+
+    # save graph object to file
+    net_file_ctr = fc_path + '{0}_ctr_net'.format(sub)
+    pickle.dump(G_ctr, open('{0}.pickle'.format(net_file_ctr), 'wb'))
+    net_file_a5ia = fc_path + '{0}_a5ia_net'.format(sub)
+    pickle.dump(G_a5ia, open('{0}.pickle'.format(net_file_a5ia), 'wb'))
+
+    G_ctr = nx.to_numpy_array(G_ctr)
+    G_a5ia = nx.to_numpy_array(G_a5ia)
+
+    # get max, min and number of nodes
+    G_ctr_min = np.append(G_ctr_min, np.min(G_ctr))
+    G_ctr_max = np.append(G_ctr_max, np.max(G_ctr))
+    G_a5ia_min = np.append(G_a5ia_min, np.min(G_a5ia))
+    G_a5ia_max = np.append(G_a5ia_max, np.max(G_a5ia))
+
+    num_nodes = np.append(num_nodes, np.shape(G_ctr)[0])
+
+
+
+
+
