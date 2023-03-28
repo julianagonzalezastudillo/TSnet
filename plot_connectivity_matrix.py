d83cc9fa4e3633e5482f1900ea52bb4ffe1b4adf
julianagonzalezastudillo <68652754+julianagonzalezastudillo@users.noreply.github.com>
2023-03-28 10:28:27 +0200

    plot for both states: ctr and a5ia

--- plot_connectivity_matrix.py
+++ plot_connectivity_matrix.py
@@ -29,77 +29,78 @@ G_max = max(list(metadata['G_ctr_max']) + list(metadata['G_a5ia_max']))
 G_min = min(list(metadata['G_ctr_min']) + list(metadata['G_a5ia_min']))
 
 order = ['M2', 'AC', 'PrL', 'IL', 'DP']
+states = ['ctr', 'a5ia']
 
 cmap = matplotlib.colors.ListedColormap([(0., 0., 1.), "white", [1., .8, 0.], [1., .4, 0.], (1., 0., 0.)])
 for sub_type in subjects.keys():
     print(sub_type)
     width_ratios = [i/min(metadata['num_nodes']) for i in metadata.loc[metadata['genot'] == sub_type, 'num_nodes']]
     size_x = sum(width_ratios)
-    fig = plt.figure(figsize=(size_x + 1, 5), dpi=600)
-    gs1 = gridspec.GridSpec(1, len(width_ratios), figure=fig, width_ratios=width_ratios)
-    gs1.update(wspace=0.3, hspace=0.05)  # set the spacing between axes.
 
-    for sub, i in zip(subjects[sub_type], range(len(subjects[sub_type]))):
-        # load graph object from file
-        net_file_ctr = fc_path + '{0}_ctr_net'.format(sub)
-        G_ctr = pickle.load(open('{0}.pickle'.format(net_file_ctr), 'rb'))
+    for sub_state in states:
+        fig = plt.figure(figsize=(size_x + 1, 5), dpi=600)
+        gs1 = gridspec.GridSpec(1, len(width_ratios), figure=fig, width_ratios=width_ratios)
+        gs1.update(wspace=0.3, hspace=0.05)  # set the spacing between axes.
 
-        # order by region clusters
-        region = nx.get_node_attributes(G_ctr, "region")
-        order_sort = sorted(zip(np.arange(0, G_ctr.number_of_nodes()), G_ctr.nodes(), region.values()),
-                            key = lambda node: [order.index(node[2])])
-        order_by_region = [n[0] for n in order_sort]
+        for sub, i in zip(subjects[sub_type], range(len(subjects[sub_type]))):
+            # load graph object from file
+            net_file_ctr = fc_path + '{0}_{1}_net'.format(sub, sub_state)
+            G = pickle.load(open('{0}.pickle'.format(net_file_ctr), 'rb'))
 
-        G_ctr = nx.to_numpy_array(G_ctr)  # transform to matrix
-        G_ctr = G_ctr[:, order_by_region][order_by_region]  # reorder
+            # order by region clusters
+            region = nx.get_node_attributes(G, "region")
+            order_sort = sorted(zip(np.arange(0, G.number_of_nodes()), G.nodes(), region.values()),
+                                key = lambda node: [order.index(node[2])])
+            order_by_region = [n[0] for n in order_sort]
 
-        # G_a5ia = nx.to_numpy_array(G_a5ia)
+            G = nx.to_numpy_array(G)  # transform to matrix
+            G = G[:, order_by_region][order_by_region]  # reorder
 
-        ax = plt.subplot(gs1[i])
-        bounds = [-np.max(G_ctr) * 1/4, -1e-16, np.max(G_ctr) * 1/4, np.max(G_ctr) * 2/4, np.max(G_ctr) * 3/4, np.max(G_ctr)]
-        norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
-        im = ax.imshow(G_ctr, cmap=cmap, norm=norm)
-        ax.set_title('Sub{0}'.format(str(sub).zfill(3)), fontsize=5)
+            ax = plt.subplot(gs1[i])
+            bounds = [-np.max(G) * 1/4, -1e-16, np.max(G) * 1/4, np.max(G) * 2/4, np.max(G) * 3/4, np.max(G)]
+            norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
+            im = ax.imshow(G, cmap=cmap, norm=norm)
+            ax.set_title('Sub{0}'.format(str(sub).zfill(3)), fontsize=10, color='white')
 
-        # count nodes per region
-        region_count = Counter(region.values())
-        print(region_count)
+            # count nodes per region
+            region_count = Counter(region.values())
+            print(region_count)
 
-        # thick line between regions
-        r_sum = [0]
-        for r, s in zip(order, range(len(order))):
-            r_sum = np.append(r_sum, r_sum[s] + region_count[r])
+            # thick line between regions
+            r_sum = [0]
+            for r, s in zip(order, range(len(order))):
+                r_sum = np.append(r_sum, r_sum[s] + region_count[r])
 
-        for s in np.arange(1, len(r_sum)-1):
-            ax.axvline(r_sum[s], color='black', lw=0.4, clip_on=False, linestyle='--')
-            ax.axhline(r_sum[s], color='black', lw=0.4, clip_on=False, linestyle='--')
+            for s in np.arange(1, len(r_sum)-1):
+                ax.axvline(r_sum[s], color='black', lw=0.4, clip_on=False, linestyle='--')
+                ax.axhline(r_sum[s], color='black', lw=0.4, clip_on=False, linestyle='--')
 
-        # set xticks according to modules
-        # xticklabels = [None] * (len(r_sum) + len(order))
-        # xticklabels[::2] = r_sum
-        # xticklabels[1::2] = order
-        # xticks = xticklabels.copy()
-        # xticks[1::2] = (np.array(xticklabels[::2]) / 2)[1:]
-        ax.set_xticks(r_sum)
-        ax.set_xticklabels(r_sum, fontsize=6)
-        ax.set_yticks(r_sum)
-        ax.set_yticklabels(r_sum, fontsize=6)
+            # set xticks according to modules
+            # xticklabels = [None] * (len(r_sum) + len(order))
+            # xticklabels[::2] = r_sum
+            # xticklabels[1::2] = order
+            # xticks = xticklabels.copy()
+            # xticks[1::2] = (np.array(xticklabels[::2]) / 2)[1:]
+            ax.set_xticks(r_sum)
+            ax.set_xticklabels(r_sum, fontsize=6, color='white')
+            ax.set_yticks(r_sum)
+            ax.set_yticklabels(r_sum, fontsize=6, color='white')
 
-        # colorbar
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes('right', size = '5%', pad = 0.05)
-        cbar = fig.colorbar(im, cax=cax, orientation='vertical')
-        cbar_ticks = cbar.ax.get_yticks()
-        cbar.ax.set_yticklabels(['{:.3f}'.format(x) for x in cbar_ticks], fontsize=6)
-        cbar.outline.set_linewidth(0.6)
+            # colorbar
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes('right', size = '5%', pad = 0.05)
+            cbar = fig.colorbar(im, cax=cax, orientation='vertical')
+            cbar_ticks = cbar.ax.get_yticks()
+            cbar.ax.set_yticklabels(['{:.3f}'.format(x) for x in cbar_ticks], fontsize=6, color='white')
+            cbar.outline.set_linewidth(0.6)
 
-    # borders
-    fig.subplots_adjust(bottom=0.05)
-    fig.subplots_adjust(top=0.95)
-    fig.subplots_adjust(right=0.95)
-    fig.subplots_adjust(left=0.05)
+        # borders
+        fig.subplots_adjust(bottom=0.05)
+        fig.subplots_adjust(top=0.95)
+        fig.subplots_adjust(right=0.95)
+        fig.subplots_adjust(left=0.05)
 
-    # save
-    plt.savefig(plot_path + 'connectivity_matrices_{0}'.format(sub_type), transparent=True)
-    fig.suptitle(sub_type)
-    plt.show()
+        # save
+        fig.suptitle([sub_type, sub_state])
+        plt.savefig(plot_path + 'connectivity_matrices_{0}_{1}'.format(sub_type, sub_state), transparent=True)
+        plt.show()
