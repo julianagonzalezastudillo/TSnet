65de6a1d8bf5060dc361c06b43fb70c853567be6
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-12-19 21:38:40 -0300

    change network analysis in function with the selected attribute

--- src/net_analysis.py
+++ src/net_analysis.py
@@ -8,8 +8,8 @@ import pickle
 
 import numpy as np
 
-from config import FC_DIR, NET_DIR, STATES
-from tools import load_metadata, order_by_region
+from config import FC_DIR, NET_DIR, ATTRIBUTE, STATES
+from tools import load_metadata, order_by_attribute
 
 # get metadata
 metadata, subjects = load_metadata()
@@ -23,28 +23,36 @@ for sub_type in subjects.keys():
 
             # Per module ["M2", "AC", "PrL", "IL", "DP"]
             Xnet = {}
-            X, order_idx, order_node_name, order_region = order_by_region(G)
-            for region in np.unique(order_region):
+            X, order_idx, order_node_name, order_attribute = order_by_attribute(
+                G, order=ATTRIBUTE
+            )
+            for attribute in np.unique(order_attribute):
                 # strength intra and inter clusters
                 idx_intra = [
-                    idx for idx, r in zip(order_idx, order_region) if r == region
+                    idx for idx, r in zip(order_idx, order_attribute) if r == attribute
                 ]
                 idx_inter = [
-                    idx for idx, r in zip(order_idx, order_region) if r not in region
+                    idx
+                    for idx, r in zip(order_idx, order_attribute)
+                    if r not in attribute
                 ]
 
-                # strength in and out regions
+                # strength in and out attributes
                 strength_in = np.sum(
-                    X[idx_inter][:, idx_intra]  # in_degree = column sum of X by regions
+                    X[idx_inter][
+                        :, idx_intra
+                    ]  # in_degree = column sum of X by attributes
                 )
                 strength_out = np.sum(
-                    X[idx_intra][:, idx_inter]  # out_degree = row sum of X by regions
+                    X[idx_intra][
+                        :, idx_inter
+                    ]  # out_degree = row sum of X by attributes
                 )
                 strength_out_in_balance = (strength_out - strength_in) / (
                     strength_out + strength_in
                 )
 
-                # strength intra and inter regions
+                # strength intra and inter attributes
                 strength_intra = np.sum(X[idx_intra][:, idx_intra])
                 strength_inter = strength_in + strength_out
                 strength_inter_intra_balance = (strength_inter - strength_intra) / (
@@ -52,18 +60,18 @@ for sub_type in subjects.keys():
                 )
 
                 # Add to net metrics dict
-                Xnet[f"{region}_strength_intra"] = np.nan_to_num(
+                Xnet[f"{attribute}_strength_intra"] = np.nan_to_num(
                     strength_intra, nan=0.0
                 )
-                Xnet[f"{region}_strength_inter"] = np.nan_to_num(
+                Xnet[f"{attribute}_strength_inter"] = np.nan_to_num(
                     strength_inter, nan=0.0
                 )
-                Xnet[f"{region}_strength_inter_intra_balance"] = np.nan_to_num(
+                Xnet[f"{attribute}_strength_inter_intra_balance"] = np.nan_to_num(
                     strength_inter_intra_balance, nan=0.0
                 )
-                Xnet[f"{region}_strength_in"] = np.nan_to_num(strength_in, nan=0.0)
-                Xnet[f"{region}_strength_out"] = np.nan_to_num(strength_out, nan=0.0)
-                Xnet[f"{region}_strength_out_in_balance"] = np.nan_to_num(
+                Xnet[f"{attribute}_strength_in"] = np.nan_to_num(strength_in, nan=0.0)
+                Xnet[f"{attribute}_strength_out"] = np.nan_to_num(strength_out, nan=0.0)
+                Xnet[f"{attribute}_strength_out_in_balance"] = np.nan_to_num(
                     strength_out_in_balance, nan=0.0
                 )
 
@@ -78,6 +86,6 @@ for sub_type in subjects.keys():
             Xnet["density"] = density
 
             # Save the dictionary to a pickle file
-            file_name = NET_DIR / f"net_metric_{sub}_{sub_state}.pkl"
+            file_name = NET_DIR / f"net_metric_{sub}_{sub_state}_{attribute}.pkl"
             with open(file_name, "wb") as pickle_file:
                 pickle.dump(Xnet, pickle_file)
